21b354c19b48a11625140fbc33c6ee309edf6b41
Dguerr15 <dguerr15@ucsc.edu>
2025-10-30 11:16:29 -0700

    test2

---
+++ game.js
@@ -0,0 +1,22 @@
+"use strict"
+
+let config = {
+    parent: 'phaser-game',
+    type: Phaser.CANVAS,
+    render: {
+        pixelArt: true  // prevent pixel art from getting blurred when scaled
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false,//set to false for final product
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
+    width: 1440,
+    height: 900,
+    scene: []
+}
